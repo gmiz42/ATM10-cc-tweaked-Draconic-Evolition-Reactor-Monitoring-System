8c3cb01cd8cdc311004f0e1019c9278173229fd9
kyoji42 <g.miz42@gmail.com>
2026-04-03 22:48:23 +0900

    Update install_outside.lua

--- install_outside.lua
+++ install_outside.lua
@@ -2,8 +2,8 @@
 local base = "https://raw.githubusercontent.com/gmiz42/ATM10-cc-tweaked-Draconic-Evolition-Reactor-Monitoring-System/main/"
 
 local files = {
-    { base .. "monitor/monitor.lua", "monitor.lua" },
-    { base .. "monitor/config_monitor.lua", "config_monitor.lua" },
+    { base .. "outside/monitor.lua", "monitor.lua" },
+    { base .. "outside/config_monitor.lua", "config_monitor.lua" },
 }
 
 for _, f in ipairs(files) do
